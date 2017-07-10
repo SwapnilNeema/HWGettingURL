e4c24e662f0d0f945d580d9ced690b6a4a40d398
Swapnil Neema <neema306@gmail.com>
2017-07-10 12:14:23 +0530

    Update Prac2.scala

--- src/main/scala/Prac2.scala
+++ src/main/scala/Prac2.scala
@@ -29,15 +29,58 @@ object Prac2 extends App{
     e =>
       println("*********PYTHONSTACKTRACE*************",e)
   }*/
-  val applicationId = "application_1496843407552_0011"
-  val appAttemptId = "appattempt_1496843407552_0011_000001"
+  //val applicationId = "application_1496843407552_0011"
+  //val appAttemptId = "appattempt_1496843407552_0011_000001"
   getRCA(applicationId,appAttemptId)
   def getRCA(applicationId: String, appAttemptId: String): Unit ={
+   
+   def writeIntoDB(message: String,stackTrace: String,countJts: Int): Unit ={
+
+      val mongoClient = MongoClient()
+      val db = mongoClient("yarnLogs")
+      val collection = db("applicationDetails")
+      val ob  = MongoDBObject("appId" -> applicationId)
+      var count = countJts
+      collection.find(ob).foreach{
+        e =>{
+          val x = e.toString
+          //x=e.toString
+          val jsv = Json.parse(x)
+          val finishedTime = (jsv \\ "finishedTime").mkString("").replaceAll("\"","")
+          val finalAppStatus = (jsv \\ "finalAppStatus").mkString("").replaceAll("\"","")
+          val appType = (jsv \\"type").mkString("").replaceAll("\"","")
+          val host = (jsv \\"host").mkString("").replaceAll("\"","")
+          val appId = applicationId
+          val attemptId = appAttemptId
+          //val message = m3.mkString("\n").split("\n")(0)
+          //val stackTrace = m3
+
+
+          val jsobj = Json.obj(
+            ("appId" -> Json.toJson(appId)),
+            ("attemptId" -> Json.toJson(appAttemptId)),
+            ("host" -> Json.toJson(host)),
+            ("type" -> Json.toJson(appType)),
+            ("message" -> Json.toJson(message)),
+            ("stackTrace" -> Json.toJson(stackTrace)),
+            //("fullStackTrace" -> Json.toJson(fullStackTrace)),
+            ("finishedTime" -> Json.toJson(finishedTime)),
+            ("finalAppStatus" -> Json.toJson(finalAppStatus))
+          )
+          Insert.InsertErrorDetails(jsobj)
+          println(count)
+          println(jsobj)
+          println("\n")
+        }
+
+      }
+    }
     val lines1 = Source.fromFile("/Users/sneema/Documents/detailedLogs/"+ applicationId +"/"+ appAttemptId +"/stdoutLog.txt").getLines().mkString("\n")
     val patternJ = ".+Exception[^\\n]++(\\s+at .++)+".r
     val m1 = patternJ.findAllIn(lines1).toList
     val patternP = "Traceback[^\\n]+\\s(\\s*File.*\\s+(.*\\s)?((ERROR|error|IndexError):.*\\s)*)+".r
     val m2 = patternP.findAllIn(lines1).toList
+    val patternJTS = "(\\d{2}\\/\\d{2}\\/\\d{2} \\d{2}:\\d{2}:\\d{2})(.*\\n.*+(\\s+at .++)+)".r
     val m3 = patternJTS.findAllIn(lines1).toList
     val writer3 = new PrintWriter("/Users/sneema/Documents/detailedLogs/"+ applicationId +"/"+ appAttemptId+"/javaStackTraceTimeStamp.txt")
     val writer1 = new PrintWriter("/Users/sneema/Documents/detailedLogs/"+ applicationId +"/"+ appAttemptId+"/javaStackTrace.txt")
@@ -58,9 +101,15 @@ object Prac2 extends App{
     m2.foreach{
       e =>
         {println("***PYTHONSTACKTRACE***",e)
+//        val sp = e.split("\n")
+          val message = sp(1)+sp(2)
+          //println("messss = "+message)
+          val stackT = e
+          writeIntoDB(message,stackT,countP)
+
 //        val writer = new PrintWriter("/Users/sneema/Documents/detailedLogs/"+ applicationId +"/"+ appAttemptId+"/pythonStackTrace.txt")
-          writer2.write(countP.toString+"\n")
-        writer2.write(e)
+        /*writer2.write(countP.toString+"\n")
+        writer2.write(e)*/
         countP=countP+1
         }
         //writer.close()}
